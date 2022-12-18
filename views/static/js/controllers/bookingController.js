f01efddb0810ad52adc13dc7aa1c12817548d56d
sand050965 <sand050965@gmail.com>
2022-12-18 15:43:28 +0800

    fix(Wrong File Name Issue): Fixed UserAuthController import file name for BookingController.js

--- views/static/js/controllers/bookingController.js
+++ views/static/js/controllers/bookingController.js
@@ -1,6 +1,6 @@
 import BookingModel from "../models/bookingModel.js";
 import BookingView from "../views/bookingView.js";
-import UserAuthController from "./UserAuthController.js";
+import UserAuthController from "./userAuthController.js";
 import UserAuthView from "../views/userAuthView.js";
 import { dateValidate } from "../utils/validatorUtil.js";
 
